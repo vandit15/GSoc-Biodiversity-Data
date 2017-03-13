b60b7c714ecbd1b637ec8612f89fc92115d908e6
vandit15 <jainvandit15@gmail.com>
2017-03-14 00:47:17 +0530

    seperating DwCterms to form list

--- further work/DwCpackage/R/getDwCterms.R
+++ further work/DwCpackage/R/getDwCterms.R
@@ -8,6 +8,19 @@ require(xml2)
 
 getDwCTerms = function() {
   dwctermsPage = httr::GET("http://rs.tdwg.org/dwc/terms/index.htm")
-    DwCTerms<-xml2::xml_text(xml2::xml_find_all(x=xml2::read_html(dwctermsPage), xpath='//p//a'))
+  DwCTerms<-xml2::xml_text(xml2::xml_find_all(x=xml2::read_html(dwctermsPage), xpath='//p//a'))
+  #removing irrelevent terms
+  DwCTerms <- DwCTerms[3:197]
+  #DwCTerms <<- DwCTerms[-which(DwCTerms == "")]
+  naindices <- which(DwCTerms == "")
+  print(naindices)
+  previ <- 0
+  termslist  <- list()
+  for(i in naindices){
+    termslist <- c(termslist,list(DwCTerms[previ+1:i-1]))
+    previ <- i
+  }
+  termslist <- list(termslist,list(DwCTerms[i+1:length(DwCTerms)]))
 
+  return(termslist)
 }
